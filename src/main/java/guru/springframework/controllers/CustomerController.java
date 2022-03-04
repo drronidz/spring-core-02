406143e9e778012655774ba86c23f54d26d003e2
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-03-04 15:53:34 +0100

    setting up property validation for (CustomerForm.userName).closes #9.

--- src/main/java/guru/springframework/controllers/CustomerController.java
+++ src/main/java/guru/springframework/controllers/CustomerController.java
@@ -6,12 +6,9 @@ import guru.springframework.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.validation.Valid;
 
 /**
  * Created by jt on 11/15/15.
@@ -41,21 +38,18 @@ public class CustomerController {
 
     @RequestMapping("/edit/{id}")
     public String edit(@PathVariable Integer id, Model model){
-        model.addAttribute("customerForm", customerService.getById(id));
+        model.addAttribute("customer", customerService.getById(id));
         return "customer/customerform";
     }
 
     @RequestMapping("/new")
     public String newCustomer(Model model){
-        model.addAttribute("customerForm", new CustomerForm());
+        model.addAttribute("customer", new CustomerForm());
         return "customer/customerform";
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String saveOrUpdate(@Valid CustomerForm customerForm, BindingResult bindingResult){
-        if (bindingResult.hasErrors()) {
-            return "customer/customerform";
-        }
+    public String saveOrUpdate(CustomerForm customerForm){
         Customer newCustomer = customerService.saveOrUpdateCustomerForm(customerForm);
         return "redirect:customer/show/" + newCustomer.getId();
     }
