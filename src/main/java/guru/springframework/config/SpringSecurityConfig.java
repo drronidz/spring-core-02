d07a0a8f53335bb61cb595e751b342ae5a309a38
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-03-05 12:58:57 +0100

    setting Dao authentication provider.

--- src/main/java/guru/springframework/config/SpringSecurityConfig.java
+++ src/main/java/guru/springframework/config/SpringSecurityConfig.java
@@ -2,9 +2,13 @@ package guru.springframework.config;
 
 import org.jasypt.springsecurity3.authentication.encoding.PasswordEncoder;
 import org.jasypt.util.password.StrongPasswordEncryptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -20,11 +24,14 @@ DATE : 3/5/2022 12:14 PM
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // No Longer authentication needed on the root url ...
-        http.authorizeRequests().antMatchers("/").permitAll();
+    private AuthenticationProvider authenticationProvider;
+
+    @Autowired
+    @Qualifier("daoAuthenticationProvider")
+    public void setAuthenticationProvider(AuthenticationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
     }
+
 
     @Bean
     public PasswordEncoder passwordEncoder(StrongPasswordEncryptor passwordEncryptor) {
@@ -33,8 +40,24 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         return passwordEncoder;
     }
 
-//    @Bean
-//    public DaoAuthenticationProvider daoAuthenticationProvider(PasswordEncoder passwordEncoder, UserDetailsService userDetailsService) {
-//
-//    }
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(PasswordEncoder passwordEncoder,
+                                                               UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        return daoAuthenticationProvider;
+    }
+
+    @Autowired
+    public void configureAuthManager(AuthenticationManagerBuilder authenticationManagerBuilder) {
+        authenticationManagerBuilder.authenticationProvider(authenticationProvider);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // No Longer authentication needed on the root url ...
+        http.authorizeRequests().antMatchers("/").permitAll();
+    }
+
 }
