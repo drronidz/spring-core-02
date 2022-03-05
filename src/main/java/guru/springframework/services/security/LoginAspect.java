39fd8c6a0b3f98e0c74700e91952a75d51466251
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-03-05 17:40:20 +0100

    using AOP in Spring (simple example).

---
+++ src/main/java/guru/springframework/services/security/LoginAspect.java
@@ -0,0 +1,38 @@
+package guru.springframework.services.security;
+
+/*
+PROJECT NAME : Spring Core Advanced - Beyond the Basics (Part 2)
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 3/5/2022 5:28 PM
+*/
+
+import org.aspectj.lang.annotation.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoginAspect {
+
+    @Pointcut("execution(* org.springframework.security.authentication.AuthenticationProvider.authenticate(..))")
+    public void doAuthenticate() {
+
+    }
+
+    @Before("guru.springframework.services.security.LoginAspect.doAuthenticate() && args(authentication)")
+    public void logBeforeAuthenticate(Authentication authentication) {
+        System.out.println("This is before the Authentication Method: authentication: " + authentication.isAuthenticated());
+    }
+
+    @AfterReturning(value = "guru.springframework.services.security.LoginAspect.doAuthenticate()", returning = "authentication")
+    public void logAfterAuthenticate(Authentication authentication) {
+        System.out.println("This is after the Authentication Method: authentication: " + authentication.isAuthenticated());
+    }
+
+    @AfterThrowing("guru.springframework.services.security.LoginAspect.doAuthenticate() && args(authentication)")
+    public void logAuthenticationException(Authentication authentication) {
+        String userDetails = (String) authentication.getPrincipal();
+        System.out.println("Login failed for user: " + userDetails);
+    }
+}
