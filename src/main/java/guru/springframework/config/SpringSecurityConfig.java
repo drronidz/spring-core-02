e2e457065fc3b6e258a94b1c14b5470ffc99cf56
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-03-05 12:28:43 +0100

    setting Spring Security Password Encoder.

--- src/main/java/guru/springframework/config/SpringSecurityConfig.java
+++ src/main/java/guru/springframework/config/SpringSecurityConfig.java
@@ -1,8 +1,14 @@
 package guru.springframework.config;
 
+import org.jasypt.springsecurity3.authentication.encoding.PasswordEncoder;
+import org.jasypt.util.password.StrongPasswordEncryptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 
 /*
 PROJECT NAME : Spring Core Advanced - Beyond the Basics (Part 2)
@@ -18,5 +24,17 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // No Longer authentication needed on the root url ...
         http.authorizeRequests().antMatchers("/").permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(StrongPasswordEncryptor passwordEncryptor) {
+        PasswordEncoder passwordEncoder = new PasswordEncoder();
+        passwordEncoder.setPasswordEncryptor(passwordEncryptor);
+        return passwordEncoder;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(PasswordEncoder passwordEncoder, UserDetailsService userDetailsService) {
+
     }
 }
