7e7bf3c912098af03dcea27d2e0990fba64fc311
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-03-07 23:39:51 +0100

    setting Thymeleaf Configuration ...

---
+++ src/main/java/guru/springframework/config/ThymeleafConfig.java
@@ -0,0 +1,60 @@
+package guru.springframework.config;
+
+/*
+PROJECT NAME : Spring Core Advanced - Beyond the Basics (Part 2)
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 3/7/2022 11:28 PM
+*/
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.resourceresolver.SpringResourceResourceResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public TemplateResolver defaultTemplateResolver() {
+        TemplateResolver resolver = new TemplateResolver();
+        resolver.setResourceResolver(thymeleafResourceResolver());
+        resolver.setPrefix("classpath:/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setCacheable(true);
+        return resolver;
+    }
+
+    @Bean
+    public SpringResourceResourceResolver thymeleafResourceResolver() {
+        return new SpringResourceResourceResolver();
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine(TemplateResolver defaultTemplateResolver) {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.addTemplateResolver(defaultTemplateResolver);
+        return engine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver thymeleafViewResolver(SpringTemplateEngine templateEngine) {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine);
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setContentType("text/html");
+        resolver.setOrder(Ordered.LOWEST_PRECEDENCE - 5);
+        return resolver;
+    }
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
+}
