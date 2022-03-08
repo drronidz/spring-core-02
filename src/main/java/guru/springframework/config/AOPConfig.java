d91b7e9e93d08e7ec1064327558a6843bdbb2198
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-03-08 11:48:08 +0100

    setting Spring AOP Configuration (disable spring boot properties) ...

---
+++ src/main/java/guru/springframework/config/AOPConfig.java
@@ -0,0 +1,15 @@
+package guru.springframework.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/*
+PROJECT NAME : Spring Core Advanced - Beyond the Basics (Part 2)
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 3/8/2022 11:46 AM
+*/
+@Configuration
+@EnableAspectJAutoProxy
+public class AOPConfig {
+}
